961f39479e1221e1914780fa986ee897adef5b22
orechen422dazo <jboy422hobby@gmail.com>
2024-09-18 07:42:50 +0900

    Initial Commit

---
+++ HealthKitApp/HealthKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitAppApp.swift
+//  HealthKitApp
+//
+//  Created by 橋本純一 on 2024/09/18.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
